9cd2a0c45d9603f13d36fba724cdb3e3e23486fa
Paulo Teixeira <pauloteixeira.ti@hotmail.com>
2025-02-28 22:53:35 -0300

    adding css in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -23,5 +23,14 @@ module.exports = {
   plugins: [new HtmlWebpackPlugin({
     template: path.resolve(__dirname, "index.html"),
     favicon: path.resolve("src", "assets", "scissors.svg")
-  })]
+  })],
+
+  module:{
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  }
 }
