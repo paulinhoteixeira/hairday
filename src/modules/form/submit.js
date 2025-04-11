eff52e4173d78d21cd772b29dba30f7717a89b57
Paulo Teixeira <pauloteixeira.ti@hotmail.com>
2025-04-10 22:13:32 -0300

    function to capture the schedule

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,6 +1,7 @@
 import dayjs from "dayjs"
 
 const form = document.querySelector('form')
+const clientName = document.getElementById("client")
 const selectedDate = document.getElementById("date")
 
 const inputToday = dayjs(new Date()).format("YYYY-MM-DD")
@@ -12,5 +13,36 @@ selectedDate.min = inputToday
 form.onsubmit = (event) => {
   event.preventDefault()
 
-  console.log("enviado")
+  try {
+    //Recupera o nome do cliente
+    const name = clientName.value.trim()
+
+    if(!name){
+      return alert("Informe o nome do cliente")
+    }
+
+    //Recupera o horario selecionado
+    const hourSelected = document.querySelector(".hour-selected")
+
+    if(!hourSelected){
+      return alert("Selecione a hora")
+    }
+    
+    //Recupera somente a hora
+    const [hour] = hourSelected.innerText.split(":")
+
+    //Inserir a hora na data
+    const when = dayjs(selectedDate.value).add(hour, "hour")
+
+    const id = new Date().getTime()
+
+    console.log({
+      id,
+      name,
+      when
+    })
+  } catch (error) {
+    alert("Não foi possível realizar o agendamento.")
+    
+  }
 }
