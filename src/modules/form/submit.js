6aabef9bc50ca3eea7681c1e6328100321e03288
Paulo Teixeira <pauloteixeira.ti@hotmail.com>
2025-04-15 23:18:03 -0300

    reload schedule after adding a new one

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,6 +1,7 @@
 import dayjs from "dayjs"
 
 import { scheduleNew } from "../../services/schedule-new.js"
+import { schedulesDay } from "../schedules/load.js"
 
 const form = document.querySelector('form')
 const clientName = document.getElementById("client")
@@ -43,6 +44,13 @@ form.onsubmit = async (event) => {
       name,
       when
     })
+
+    //Recarregar o horario selecionado
+    await schedulesDay()
+
+    //Limpar o input de nome do cliente
+    clientName.value = ""
+
   } catch (error) {
     alert("Não foi possível realizar o agendamento.")
     
